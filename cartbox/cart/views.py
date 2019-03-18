0447196ba1f17cda3646b03cc1af0678827d9e70
Ben Ayers-Glassey <bayersglassey@gmail.com>
2019-03-18 08:44:35 -0700

    towards suggested products...

--- cartbox/cart/views.py
+++ cartbox/cart/views.py
@@ -5,29 +5,65 @@ from django import forms
 
 from .models import Category, Product, Order
 
+from analytics.stats import StatsMixin
 
-class ShopView(CreateView):
+
+class ShopView(StatsMixin, CreateView):
     model = Order
     fields = []
     template_name = 'cart/shop.html'
     success_url = '/cart/order/{id}/'
-    def get_context_data(self):
-        context = super().get_context_data()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
         context['categories'] = Category.objects.all()
+        context['suggested_products'] = self.get_suggested_products()
         return context
+
+    def get_suggested_product(self, user, product):
+        # TODO: Use StatsMixin to figure out which product which isn't
+        # already in the cart has the highest chance of user clicking it.
+        return product
+
+    def get_suggested_products(self):
+        request = self.request
+        user = request.user
+        cart_products = self.get_cart_products()
+        suggested_products = []
+        for product in cart_products:
+            suggested_product = self.get_suggested_product(
+                user, product)
+            suggested_products.append(suggested_product)
+        return suggested_products
+
+    def get_cart_products(self):
+        request = self.request
+        user = request.user
+        skus = request.POST.getlist('sku')
+        suggested_skus = request.POST.getlist('suggested-sku')
+        products = list(Product.objects.filter(sku__in=skus))
+        suggested_products = list(Product.objects.filter(
+            sku__in=suggested_skus))
+        for product in suggested_products:
+            # Secret attribute poked onto the object
+            product._suggested = True
+        return products + suggested_products
+
     def form_valid(self, form):
         request = self.request
         user = request.user
-        order = user.orders.create()
-        for key, values in request.POST.lists():
-            if key not in ['sku', 'suggested-sku']: continue
-            suggested = key == 'suggested-sku'
-            products = Product.objects.filter(sku__in=values)
+
+        if 'place-order' in request.POST:
+            order = user.orders.create()
+            products = self.get_cart_products()
             for product in products:
+                suggested = getattr(product, '_suggested', False)
                 item = order.add_item(product, suggested=suggested)
-        order.place()
-        self.object = order
-        return FormMixin.form_valid(self, form)
+            order.place()
+            self.object = order
+            return FormMixin.form_valid(self, form)
+
+        return self.form_invalid(form)
 
 class UserOrdersViewMixin:
     model = Order
