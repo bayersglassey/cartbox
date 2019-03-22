72dda0bd8547016c258ee15e591eb0ea76d317b2
Ben Ayers-Glassey <bayersglassey@gmail.com>
2019-03-21 23:04:03 -0700

    deploy scripts & uwsgi config now support static files

--- bump.sh
+++ bump.sh
@@ -2,4 +2,4 @@
 set -e
 
 scp workon cartbox@"$IP":
-ssh cartbox@"$IP" '. workon && git pull && pip install -r requirements.txt && python manage.py migrate'
+ssh cartbox@"$IP" '. workon && git pull && pip install -r requirements.txt && python manage.py collectstatic --no-input && python manage.py migrate'
