25dc6635867dc2d0f67c4b1c903d5584e576f67e
Ben Ayers-Glassey <bayersglassey@gmail.com>
2019-03-21 22:46:01 -0700

    fixed STATIC_ROOT, STATICFILES_DIRS

--- cartbox/cartbox/settings.py
+++ cartbox/cartbox/settings.py
@@ -120,7 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'site_assets')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'site_assets')]
 
 
 from local_settings import *
