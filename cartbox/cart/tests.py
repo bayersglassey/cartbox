4d9edf8e833a907cc7d6abf7ae8d1f65037ff687
Ben Ayers-Glassey <bayersglassey@gmail.com>
2019-11-21 21:26:42 -0800

    cart: added documentation everywhere

--- cartbox/cart/tests.py
+++ cartbox/cart/tests.py
@@ -9,6 +9,8 @@ from . import views
 
 
 class CartRequestFactory(RequestFactory):
+    """Request factory which pokes stuff onto the request so we
+    don't need to run middleware"""
 
     def __init__(self, user, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -27,6 +29,8 @@ class CartTestCaseMixin:
 
         self.factory = CartRequestFactory(self.user)
 
+        # Create default products & categories, poke some onto self
+        # so we can place orders with specific items
         update_or_create_cats_and_prods(verbose=False)
         self.fruit = Category.objects.get(title="Fruit")
         self.veg = Category.objects.get(title="Veg")
@@ -50,6 +54,7 @@ class CartTestCaseMixin:
 class CartTestCase(CartTestCaseMixin, TestCase):
 
     def test_order_place(self):
+        # Place an order
         order = self.user.place_order([self.banana, self.apple])
 
         # We should get 3 counters:
@@ -57,23 +62,23 @@ class CartTestCase(CartTestCaseMixin, TestCase):
         #  <SKUInOrderCounter: 1 x 4101 1549>
         #  <SKUPairInOrderCounter: 1 x 4011 1549 | 4101 1549>
         counters = order._placed_counters
-        self.assertEqual(len(counters), 3)
+        self.assertLen(counters, 3)
 
         # Check the SKUInOrderCounters
         sku_counters = [c for c in counters
             if isinstance(c, SKUInOrderCounter)]
-        self.assertEqual(len(sku_counters), 2)
+        self.assertLen(sku_counters, 2)
         banana_counters = [c for c in sku_counters
             if c.sku == self.banana.sku]
-        self.assertEqual(len(banana_counters), 1)
+        self.assertLen(banana_counters, 1)
         apple_counters = [c for c in sku_counters
             if c.sku == self.apple.sku]
-        self.assertEqual(len(apple_counters), 1)
+        self.assertLen(apple_counters, 1)
 
         # Check the SKUPairInOrderCounter
         sku_pair_counters = [c for c in counters
             if isinstance(c, SKUPairInOrderCounter)]
-        self.assertEqual(len(sku_pair_counters), 1)
+        self.assertLen(sku_pair_counters, 1)
         sku_pair_counter = sku_pair_counters[0]
         # Note: SKUPairInOrderCounter guarantees sku1 < sku2.
         # banana.sku is "4011", apple.sku is "4101".
@@ -86,19 +91,20 @@ class CartTestCase(CartTestCaseMixin, TestCase):
             order.place()
 
     def test_order_items_by_category(self):
+        # Place an order, get its items_by_category
         order = self.user.place_order([self.banana, self.apple, self.beef])
         items_by_category = order.items_by_category()
         self.assertEqual(set(items_by_category), {self.fruit, self.meat})
 
         # Check fruit items
         fruit_items = items_by_category[self.fruit]
-        self.assertEqual(len(fruit_items), 2)
+        self.assertLen(fruit_items, 2)
         self.assertEqual({i.sku for i in fruit_items},
             {self.banana.sku, self.apple.sku})
 
         # Check meat item
         meat_items = items_by_category[self.meat]
-        self.assertEqual(len(meat_items), 1)
+        self.assertLen(meat_items, 1)
         self.assertEqual({i.sku for i in meat_items},
             {self.beef.sku})
 
@@ -125,6 +131,8 @@ class CartViewsTestCase(CartTestCaseMixin, TestCase):
     def setUp(self):
         super().setUp()
 
+        # ...I kind of prefer just using self.client instead of calling
+        # views by hand. But that's not the approach we took here. Ah well.
         self.shop_view = views.ShopView.as_view()
         self.order_view = views.OrderView.as_view()
         self.orders_view = views.OrdersView.as_view()
@@ -135,6 +143,7 @@ class CartViewsTestCase(CartTestCaseMixin, TestCase):
         response = self.shop_view(request)
         self.assertEqual(response.status_code, 200)
 
+        # POST to shop view (but don't place order, just show "cart")
         request = self.factory.post('fake_url', {
             'sku': self.banana.sku,
             'sku': self.apple.sku,
@@ -142,7 +151,14 @@ class CartViewsTestCase(CartTestCaseMixin, TestCase):
         })
         response = self.shop_view(request)
         self.assertEqual(response.status_code, 200)
+        context = response.context_data
+        # Make sure context has a list of suggested products (but didn't
+        # find any suggestions, because we didn't set user up with any
+        # analytics counters...)
+        self.assertLen(context["suggested_products"], 0)
 
+        # POST to shop view, placing an order (by "clicking the place-order
+        # submit button", as it were)
         request = self.factory.post('fake_url', {
             'sku': self.banana.sku,
             'sku': self.apple.sku,
@@ -150,6 +166,7 @@ class CartViewsTestCase(CartTestCaseMixin, TestCase):
             'place-order': '',
         })
         response = self.shop_view(request)
+        # Make sure an order was placed & we were redirected to its page
         self.assertStartsWith(response['Location'], '/cart/order/')
 
     def test_order_view(self):
--- cartbox/cart/views.py
+++ cartbox/cart/views.py
@@ -9,6 +9,17 @@ from analytics.stats import Stats
 
 
 class ShopView(CreateView):
+    """The main e-commerce view.
+    Shows user all the products, organized by category in a multi-select
+    widget.
+    There are 2 submit buttons, each resulting in a different behaviour
+    on POST:
+        * Update Cart: reloads the page showing user suggested products
+            based on the ones they had selected
+        * Place Order: places an order using products user selected, and
+            redirects to order's page
+    """
+
     model = Order
     fields = []
     template_name = 'cart/shop.html'
@@ -21,6 +32,8 @@ class ShopView(CreateView):
         return context
 
     def get_suggested_product(self, user, cart_products, product):
+        """Using analytics for given user and their selected products,
+        returns a suggested product or None."""
         # TODO: Make this less crappy.
         # Although really, this method shouldn't exist at all...
         # get_suggested_products should combine the suggestions for
@@ -34,6 +47,8 @@ class ShopView(CreateView):
         return None
 
     def get_suggested_products(self):
+        """Using analytics for user and their selected products, returns
+        a list of suggested products"""
         request = self.request
         user = request.user
         cart_products = self.get_cart_products()
@@ -46,16 +61,26 @@ class ShopView(CreateView):
         return suggested_products
 
     def get_cart_products(self):
+        """Returns list of products, based on the skus in request.POST"""
         request = self.request
         user = request.user
+
+        # There are 2 multi-select widgets, one showing all skus organized
+        # by category, the other showing suggested skus
         skus = request.POST.getlist('sku')
         suggested_skus = request.POST.getlist('suggested-sku')
+
+        # Get products & suggested products from database
         products = list(Product.objects.filter(sku__in=skus))
         suggested_products = list(Product.objects.filter(
             sku__in=suggested_skus))
+
+        # Poke a secret attribute onto suggested products so we can tell
+        # them apart
         for product in suggested_products:
-            # Secret attribute poked onto the object
             product._suggested = True
+
+        # Return all products as a single list
         return products + suggested_products
 
     def form_valid(self, form):
@@ -63,18 +88,26 @@ class ShopView(CreateView):
         user = request.user
 
         if 'place-order' in request.POST:
+            # User clicked "Place Order".
+            # Place an order & redirect to its page.
             order = user.orders.create()
             products = self.get_cart_products()
             for product in products:
+                # Check for secret attribute poked on by get_cart_products
                 suggested = getattr(product, '_suggested', False)
                 item = order.add_item(product, suggested=suggested)
             order.place()
             self.object = order
             return FormMixin.form_valid(self, form)
 
+        # User clicked "Update Cart".
+        # Just re-render the page, get_context_data() and friends will
+        # peek at request.POST and send the template a list of suggested
+        # products to render
         return self.form_invalid(form)
 
 class UserOrdersViewMixin:
+    """Show all orders for request.user"""
     model = Order
     def get_queryset(self):
         request = self.request
@@ -90,6 +123,7 @@ class OrderView(UserOrdersViewMixin, DetailView):
 
 
 class ClearAccountView(FormView):
+    """Deletes request.user's orders and analytics counters on POST"""
     template_name = 'cart/clear_account.html'
     form_class = forms.Form
     success_url = '/'
